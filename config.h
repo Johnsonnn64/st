77f3e5465659ec686b946bccb77a602919f0ac1f
Johnsonnn64 <naktacamel2@gmail.com>
2022-08-31 20:29:53 +0800

    jetbrainsmono font name change

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "JetBrainsMono Medium Nerd Font:size=11.5:antialias=true:autohint=true";
+static char *font = "JetBrainsMono Nerd Font:size=11.5:antialias=true:autohint=true";
 static char *font2[] = { "JoyPixels:size=11.5:antialias=true:autohint=true" };
 static int borderpx = 1;
 
